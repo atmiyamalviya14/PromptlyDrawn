db7a3bacb9d1da9d6fdbeb728a0ac7e1a4a80e24
atmiyamalviya14 <malviyaatmiya0@gmail.com>
2025-05-30 23:33:24 +0530

    final

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,36 +4,40 @@ import bcrypt from 'bcrypt'
 import razorpay from 'razorpay'
 import transactionModel from '../models/transactionModel.js'
 
-const registerUser = async (req, res)=>{
-    try {
-        const {name, email, password} = req.body
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-        if(!name || !email || !password){
-            return res.json({success:false, message: 'Missing Details'})
-        }
-
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password, salt)
-
-        const userData = {
-            name, 
-            email, 
-            password: hashedPassword
-        }
-
-        const newUser = new userModel(userData)
-        const user = await newUser.save()
-
-        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
-
-        res.json({success: true, token, user:{name:user.name}})
-
-
-    } catch (error) {
-        console.log(error)
-        res.json({success: false, message: error.message})
+    if (!name || !email || !password) {
+      return res.json({ success: false, message: 'Missing Details' });
     }
-}
+
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
+      return res.json({ success: false, message: 'User already exists' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const userData = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    res.json({ success: true, token, user: { name: user.name } });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
 
 const loginUser = async (req, res)=>{
     try {
